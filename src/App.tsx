26cc65cd4a27aca3302572cee3429a4fe2216397
yu nishisaki <ynishisaki@gmail.com>
2022-12-01 22:44:52 +0900

    add:entity追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-// import React from "react";
-// import logo from "./logo.svg";
-// import "./App.css";
-import { Viewer } from "resium";
+import { Viewer, Entity, PointGraphics, EntityDescription } from "resium";
+import { Cartesian3, createWorldTerrain } from "cesium";
+
+const terrainProvider = createWorldTerrain();
+const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100);
 
 function App() {
-    return <Viewer />;
+    return (
+        <Viewer full terrainProvider={terrainProvider}>
+            <Entity position={position} name='Tokyo'>
+                <PointGraphics pixelSize={10} />
+                <EntityDescription>
+                    <h1>マークダウンかけちゃうよ</h1>
+                    <p>Population: 9,273,000</p>
+                    <h2>やっほい</h2>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                </EntityDescription>
+            </Entity>
+        </Viewer>
+    );
 }
 
 export default App;
