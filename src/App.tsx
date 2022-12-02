2ae4f840d0528ba0c8cde82d283c632dcdab32cc
yu nishisaki <ynishisaki@gmail.com>
2022-12-02 16:40:24 +0900

    add:PLATEAUオルソを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,35 @@
-import { Viewer, Entity, PointGraphics, EntityDescription } from "resium";
+import {
+    Viewer,
+    Entity,
+    PointGraphics,
+    EntityDescription,
+    ImageryLayer,
+} from "resium";
 import { Cartesian3 } from "cesium";
 import * as Cesium from "cesium";
 
 // Cesium Ionの読み込み指定
-if (typeof process.env.REACT_APP_CESIUM_ACCESS_TOKEN != "undefined") {
-    Cesium.Ion.defaultAccessToken = process.env.REACT_APP_CESIUM_ACCESS_TOKEN;
-}
+Cesium.Ion.defaultAccessToken = process.env.REACT_APP_CESIUM_ACCESS_TOKEN!; // もうちょっとよい書き方ないかな
 
 // Terrainの指定（EGM96、国土数値情報5m標高から生成した全国の地形モデル、5m標高データが無い場所は10m標高で補完している）
 const terrainProvider = new Cesium.CesiumTerrainProvider({
-    url: process.env.REACT_APP_CESIUM_ASSET_ID
-        ? Cesium.IonResource.fromAssetId(
-              parseInt(process.env.REACT_APP_CESIUM_ASSET_ID)
-          )
-        : "https://assets.agi.com/stk-terrain/world",
-    // undefinedの判定方法をもうちょっとどうにかしたい
-    // url: Cesium.IonResource.fromAssetId(
-    //     process.env.REACT_APP_CESIUM_ASSET_ID
-    //         ? parseInt(process.env.REACT_APP_CESIUM_ASSET_ID)
-    //         : 1
-    // ),
+    url: Cesium.IonResource.fromAssetId(
+        parseInt(process.env.REACT_APP_CESIUM_ASSET_ID!)
+    ),
 });
+
+// G空間情報センターに置かれている、Project PLATEAUで作成したPLATEAUオルソの参照
+const imageProvider = new Cesium.UrlTemplateImageryProvider({
+    url: process.env.REACT_APP_CESIUM_ORTHO_IMAGE_TILE_URL!,
+    maximumLevel: 19,
+});
+
 const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100);
 
 function App() {
     return (
         <Viewer full terrainProvider={terrainProvider}>
+            <ImageryLayer imageryProvider={imageProvider} />
             <Entity position={position} name='Tokyo'>
                 <PointGraphics pixelSize={10} />
                 <EntityDescription>
